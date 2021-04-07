21f0d16dc4704d48222687851a9a700c3c6051bf
Luis Manzo <manzosk3@gmail.com>
2021-04-07 13:57:38 -0400

    sets up dog similar to cat object, creates create prairedog object with function howl and uses dog's sound via prototypal inheritance

--- object-creation/funfun-46-prototype-basics.js
+++ object-creation/funfun-46-prototype-basics.js
@@ -10,5 +10,19 @@ let cat = {
   sound: 'meow'
 }
 
+let dog = {
+  sound: 'woof'
+}
+
 Object.setPrototypeOf(cat, animal);
 cat.talk();
+
+let prairedog = {
+  howl: function() {
+    console.log(this.sound.toUpperCase());
+  }
+}
+
+Object.setPrototypeOf(prairedog, dog);
+
+prairedog.howl();
