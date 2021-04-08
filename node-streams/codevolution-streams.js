0381855419ed924c4741134d7453fa7f6e60d0be
Luis Manzo <manzosk3@gmail.com>
2021-04-08 19:47:45 -0400

    declares hardcoded write data, uses writeStream to write to output.txt

--- node-streams/codevolution-streams.js
+++ node-streams/codevolution-streams.js
@@ -11,3 +11,11 @@ readableStream.on('data', function(chunk) {
 readableStream.on('end', function() {
   console.log(data);
 })
+
+let writeData = 'hello world';
+let writableStream = fs.createWriteStream(`${__dirname}/output.txt`);
+writableStream.write(writeData, 'utf8');
+writableStream.end();
+writableStream.on('finish', function() {
+  console.log('write completed');
+})
