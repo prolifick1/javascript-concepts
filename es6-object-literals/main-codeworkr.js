cd644c7f56fb8d1ac084e4417b6baf9848e97a22
Luis Manzo <manzosk3@gmail.com>
2021-04-14 16:57:52 -0400

    changes print function to be inside object

--- es6-object-literals/main-codeworkr.js
+++ es6-object-literals/main-codeworkr.js
@@ -2,15 +2,13 @@ const name = 'John';
 const title = 'Developer';
 const salary = 120000;
 
-const print = function() {
-  console.log(`my name is ${this.name}`);
-}
-
 const developer = {
   name,
   title,
   salary,
-  print
+  print() {
+    console.log(`my name is ${this.name}`);
+  }
 }
 
 developer.print();
